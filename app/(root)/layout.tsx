3a1805f615e5cb2f59961357362ef6f51ba14595
Riddhi Gupta <riddhii.gupta2103@gmail.com>
2024-08-10 12:37:49 +0530

    implement initial file and folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <main>
+        SIDEBAR
+        {children}
+    </main>
+  );
+}
+
+//home,transcation etc. All pages that use a sidebar.
